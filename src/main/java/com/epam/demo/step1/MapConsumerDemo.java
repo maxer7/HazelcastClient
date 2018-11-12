0f899c6e1f7154d280c0827e70d7df6f907b8409
Maksym_Oleshchuk <Maksym_Oleshchuk@epam.com>
2018-11-12 19:51:00 +0200

    Code prettify

--- src/main/java/com/epam/demo/step1/MapConsumerDemo.java
+++ src/main/java/com/epam/demo/step1/MapConsumerDemo.java
@@ -12,13 +12,9 @@ public class MapConsumerDemo {
     private static final Logger LOGGER = LoggerFactory.getLogger(MapConsumerDemo.class);
 
     public void show(IMap<Long, Long> map) {
-        consume(map);
-    }
-
-    public void consume(IMap<Long, Long> distributedMap) {
         Stream.iterate(0, (i) -> ++i)
             .forEach(i -> {
-                LOGGER.info("Get by key {}. Value is {} ", i, distributedMap.get(i));
+                LOGGER.info("Get by key {}. Value is {} ", i, map.get(i));
                 ThreadUtils.sleepUnsafe(1000);
             });
     }
